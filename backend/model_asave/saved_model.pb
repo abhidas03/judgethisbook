4f3ff9999fd3a05f43496adb3fbab1997997e855
Abhijit Das <adas3@cs.swarthmore.edu>
2023-07-25 14:17:24 -0400

    responsive graph

--- backend/model_asave/saved_model.pb
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:465805da7ffe95a392d9a068d8d4ef2770f730055894c8b7a1bf3009fae73175
-size 427735
--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function uploadImage() {
     formData.append("image", file);
 
     //    const apiEndpoint = "backend/bookAPI.py";
-    const apiEndpoint = "https://cs.swarthmore.edu/~pdevlin2/book_site/backend/bookAPI.py"
+    const apiEndpoint = "/~pdevlin2/book_site/backend/bookAPI.py"
     fetch(apiEndpoint, {
         method: "POST",
         body: formData
@@ -23,8 +23,8 @@ function uploadImage() {
     })
     .then(data => {
         // Handle the response data here.  Put whatever you want (e.g., the javascript uses the response in data to do whatever cool updates to the page you want)
-        var responseExample = displayResults(data);
-        print(responseExample);
+        console.log(data)
+        displayResults(data['predictions']);
         alert("Server response:\n" + JSON.stringify(data, null, 2));
     })
     .catch(error => {
@@ -33,38 +33,52 @@ function uploadImage() {
     });
 }
 
-function displayResults(data) {
+function drawChart() {
+    // Create the data table.
+    
+    var data = new google.visualization.DataTable();
+    /*
+    let range = 5;
+    for (let i = 0; i < range; i++) {
+      data.addColumn('string', )
+    } 
+    */
+
+    data.addColumn('string', 'Toppings');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+      ['Mushrooms', 3],
+      ['Onions', 1],
+      ['Olives', 1],
+      ['Zucchini', 1],
+      ['Pepperoni', 2]
+    ]);
+
+   let width = document.getElementById('top').offsetWidth;
+    console.log(width);
+    // Set chart options
+    var options = {'title':'How Much Pizza I Ate Last Night',
+                //    'chartArea': {'left': '10', 'top': '20','right': '10', 'bottom': '20'},
+                   'width': .5*width,
+                   'height': .3*width,
+                   'legend': {'position': 'bottom'},
+                  };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
+
+function displayResults(predictions) {
     // Load the Visualization API and the corechart package.
     google.charts.load('current', {'packages':['corechart']});
-
+    console.log(predictions);
     // Set a callback to run when the Google Visualization API is loaded.
     google.charts.setOnLoadCallback(drawChart);
-
     // Callback that creates and populates a data table,
     // instantiates the pie chart, passes in the data and
     // draws it.
-    function drawChart() {
-
-      // Create the data table.
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Topping');
-      data.addColumn('number', 'Slices');
-      data.addRows([
-        ['Mushrooms', 3],
-        ['Onions', 1],
-        ['Olives', 1],
-        ['Zucchini', 1],
-        ['Pepperoni', 2]
-      ]);
-
-      // Set chart options
-      var options = {'title':'How Much Pizza I Ate Last Night',
-                     'width': 500,
-                     'height': 400};
-
-      // Instantiate and draw our chart, passing in some options.
-      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
-    }
+    
 }
-displayResults()
+
+window.addEventListener("resize", drawChart);
